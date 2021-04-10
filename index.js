4567f0dc1bac749d6bff401a3207b018d8a69143
Blank <pierre.blank@epitech.eu>
2021-04-10 09:02:16 +0200

    Voice recognition

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const nanoid = require('nanoid');
+const Silence = require('./Silence')
+const vosk = require('vosk');
+const { Readable, Writable } = require('stream');
+const { Mixer } = require('audio-mixer');
 const sqlite3 = require('sqlite3');
 const config = require('./config.json');
 const fs = require('fs');
@@ -7,6 +11,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 const db = new sqlite3.Database(config.dbName);
+const model = new vosk.Model("Model");
 
 db.run("CREATE TABLE IF NOT EXISTS MEME_SONG(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, PATH TEXT NOT NULL, CMD TEXT NOT NULL, SERVER TEXT NOT NULL);");
 
@@ -18,11 +23,57 @@ const download = (url, path, callback) => {
     })
 };
 
+const silence = new Silence();
+
 client.on('message', async message => {
     param = message.content.split(' ');
     //if talk directly to onii-chan he don't crash
     if (!message.guild) return;
-    if (param[0] == '!addmeme') {
+    if (param[0] == "!listen") {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            message.member.voice.channel.members.each((user, id) => {
+                if (user.user.bot) {
+                    return;
+                }
+                console.log('init stream');
+                let mixer = new Mixer({
+                    channels: 1,
+                    bitDepth: 16,
+                    sampleRate: 48000,
+                });
+                let discordInput = mixer.input({
+                    channels: 2,
+                    bitDepth: 16,
+                    sampleRate: 48000,
+                });
+
+                const rec = new vosk.Recognizer({ model: model, sampleRate: 48000.0 });
+                const audio = connection.receiver.createStream(user, { mode: 'pcm', end: 'manual' });
+
+                console.log('end init stream');
+                const parserWriter = new Writable({
+                    write(chunk, encoding, callback) {
+                        rec.acceptWaveform(chunk);
+                        let empty = true;
+                        for (let i = 0; i < chunk.length; i++) {
+                            if (chunk[i]) {
+                                empty = false;
+                                break;
+                            }
+                        }
+                        if (empty) {
+                            console.log(rec.finalResult());
+                        }
+                        callback();
+                    }
+                });
+                mixer.pipe(parserWriter);
+                audio.pipe(discordInput);
+            })
+        }
+    }
+    else if (param[0] == '!addmeme') {
         fileName = config.saveFolder + nanoid.nanoid() + '.mp3';
         while (fs.existsSync(fileName)) {
             fileName = config.saveFolder + nanoid.nanoid() + '.mp3';
@@ -43,29 +94,27 @@ client.on('message', async message => {
         })
 
     }
-    if (param[0] == '!aniki') {
+    else if (param[0] == '!aniki') {
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
             db.get('SELECT PATH FROM MEME_SONG WHERE CMD = ? AND SERVER = ?', param[1], message.guild.id.toString(), async (error, row) => {
-                if(error){
+                if (error) {
                     message.channel.send('YAMETE la Y a des blempro dans la DB !');
                     return;
                 }
-                if (!row){
+                if (!row) {
                     message.channel.send('Tasukete kure comando inconnue !');
                     return;
                 }
                 const connection = await message.member.voice.channel.join();
                 console.log(row['PATH']);
-                const dispatcher = connection.play(row['PATH'] ,{volume:0.5});
+                const dispatcher = connection.play(row['PATH'], { volume: 0.5 });
                 dispatcher.on('finish', () => {
                     console.log('Finished playing!');
                     dispatcher.destroy();
                     connection.disconnect();
                 });
             });
-
-
         }
         else {
             message.channel.send('You need to join a voice channel first!');
